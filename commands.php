9e4deac59c68a47bfb1762bcd1ae8c208787c5cb
VPDIXIT007 <74156454+VPDIXIT007@users.noreply.github.com>
2023-12-10 20:12:31 +0530

    laravel jetstream vue inertia setup with commands file

---
+++ commands.php
@@ -0,0 +1,13 @@
+composer create-project laravel/laravel example-app
+
+cd example-app
+
+composer require laravel/jetstream
+
+php artisan jetstream:install inertia
+
+php artisan jetstream:install inertia --teams
+
+npm install
+npm run build
+php artisan migrate
